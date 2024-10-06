8fd8fe3f9aa55e9bff6e3c2f47f0920d54d352f7
nguyenvietduong <duongnv10504@gmail.com>
2024-10-06 17:41:25 +0700

    NguyenVietDuong - Profile

--- resources/views/backend/component/auth.blade.php
+++ resources/views/backend/component/auth.blade.php
@@ -20,7 +20,8 @@
                 <h6 class="my-0 fw-medium text-dark fs-13">{{ Auth::user()->full_name ?? 'No data available' }}</h6>
                 <small class="text-muted mb-0">
                     @if(Auth::check())
-                    {{ Auth::user()->role_id == 1 ? __('messages.role.role.manager') : (Auth::user()->role_id == 2 ? __('messages.role.role.admin') : '') }}
+                    {{ Auth::user()->role_id == 1 ? __('messages.role.role.manager') : (Auth::user()->role_id == 2 ?
+                    __('messages.role.role.admin') : '') }}
                     @endif
                 </small>
             </div>
@@ -32,10 +33,10 @@
             {{ __('messages.system.profile') }}</a>
         {{-- <a class="dropdown-item" href="pages-faq.html"><i class="las la-wallet fs-18 me-1 align-text-bottom"></i>
             Earning</a> --}}
-        <small class="text-muted px-2 py-1 d-block">{{ __('messages.system.setting') }}</small>
+        <small class="text-muted px-2 py-1 d-block ml-5">{{ __('messages.system.setting') }}</small>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: inline;">
             @csrf
-            <button type="submit" class="dropdown-item text-danger">
+            <button type="submit" onclick="executeExample('handleDismiss', 'logout-form')" class="text-danger border-0 bg-transparent ps-3 mb-1">
                 <i class="las la-power-off fs-18 me-1 align-text-bottom"></i> {{ __('messages.system.logout') }}
             </button>
         </form>
