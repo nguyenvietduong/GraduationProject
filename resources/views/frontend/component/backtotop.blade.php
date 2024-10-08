4e7c634732461b96136dc304c8700514afa044a0
Phùng Mạnh Đức <phungmanhduc2209@gmail.com>
2024-10-08 10:00:03 +0700

    Phung Manh Duc commit

---
+++ resources/views/frontend/component/backtotop.blade.php
@@ -0,0 +1,3 @@
+<a href="#" onclick="topFunction()" id="back-to-top"
+class="back-to-top fixed hidden text-lg rounded-full z-10 bottom-5 end-5 size-9 text-center bg-amber-500 text-white justify-center items-center"><i
+    class="mdi mdi-arrow-up"></i></a>
---
+++ resources/views/frontend/component/js.blade.php
@@ -0,0 +1,7 @@
+<script src="{{ asset('frontend/assets/libs/shufflejs/shuffle.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/libs/tiny-slider/min/tiny-slider.js') }}"></script>
+<script src="{{ asset('frontend/assets/libs/tobii/js/tobii.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/libs/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/libs/swiper/js/swiper.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/plugins.init.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/app.js') }}"></script>
