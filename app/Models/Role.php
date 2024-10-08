1028da075971ec4faf7fff93ecd201d37e5b94e9
Phùng Mạnh Đức <phungmanhduc2209@gmail.com>
2024-10-08 17:45:42 +0700

    Phung Manh Duc commit

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Spatie\Permission\Models\Permission;
+use App\Models\Permission;
 
 class Role extends Model
 {
