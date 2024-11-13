0770fca06daaab3c5159deb58cc9cd96a6b91393
truc <trucbntdev7204@gmail.com>
2024-11-13 17:41:43 +0700

    trucph33131 thong ke

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -9,158 +9,133 @@
                         <div class="row d-flex justify-content-center border-dashed-bottom pb-3">
                             <div class="col-9">
                                 <p class="text-dark mb-0 fw-semibold fs-14">Số người đang sử dụng</p>
-                                <h5 class="mt-2 mb-0 fw-bold">24k</h5>
+                                <h5 class="mt-2 mb-0 fw-bold">{{number_format($data["countUser"] / 1000, 3, '.', '');}}</h5>
                             </div>
-                            <!--end col-->
                             <div class="col-3 align-self-center">
                                 <div
                                     class="d-flex justify-content-center align-items-center thumb-xl bg-light rounded-circle mx-auto">
                                     <i class="fa-solid fa-user" style="color: green"></i>
                                 </div>
                             </div>
-                            <!--end col-->
                         </div>
-                        <!--end row-->
-
                     </div>
-                    <!--end card-body-->
                 </div>
-                <!--end card-->
             </div>
-            <!--end col-->
             <div class="col-md-6 col-lg-3">
                 <div class="card">
                     <div class="card-body">
                         <div class="row d-flex justify-content-center border-dashed-bottom pb-3">
                             <div class="col-9">
                                 <p class="text-dark mb-0 fw-semibold fs-14">Món ăn</p>
-                                <h5 class="mt-2 mb-0 fw-bold">100</h5>
+                                <h5 class="mt-2 mb-0 fw-bold">{{number_format($data["countFood"], 0, '.', '.');}}</h5>
                             </div>
-                            <!--end col-->
                             <div class="col-3 align-self-center">
                                 <div
                                     class="d-flex justify-content-center align-items-center thumb-xl bg-light rounded-circle mx-auto">
                                     <i class="fa-solid fa-bowl-food" style="color: coral"></i>
                                 </div>
                             </div>
-                            <!--end col-->
                         </div>
-                        <!--end row-->
                     </div>
-                    <!--end card-body-->
                 </div>
-                <!--end card-->
             </div>
-            <!--end col-->
             <div class="col-md-6 col-lg-3">
                 <div class="card">
                     <div class="card-body">
                         <div class="row d-flex justify-content-center border-dashed-bottom pb-3">
                             <div class="col">
                                 <p class="text-dark mb-0 fw-semibold fs-14">Doanh thu tháng</p>
-                                <h5 class="mt-2 mb-0 fw-bold" ">1.000.000 VNĐ</h3>
+                                <h5 class="mt-2 mb-0 fw-bold" ">{{number_format($data["totalMonth"], 0, '.', '.');}} VNĐ</h3>
                           </div>
-                          <!--end col-->
                           <div class="col-3 align-self-center">
                             <div class="d-flex justify-content-center align-items-center thumb-xl bg-light rounded-circle mx-auto">
                               <i class="fa-regular fa-money-bill-1" style="color: green"></i>
                             </div>
                         </div>
-                          <!--end col-->
                       </div>
-                      <!--end row-->
-                  
                   </div>
-                  <!--end card-body-->
               </div>
-              <!--end card-->
             </div>
             <div class="col-md-6 col-lg-3">
                 <div class="card">
                     <div class="card-body">
                         <div class="row d-flex justify-content-center border-dashed-bottom pb-3">
                             <div class="col">
-                                <p class="text-dark mb-0 fw-semibold fs-14">Doanh Năm</p>
-                                <h5 class="mt-2 mb-0 fw-bold" ">1.000.000 VNĐ</h5>
+                                <p class="text-dark mb-0 fw-semibold fs-14">Doanh thu Năm </p>
+                                <h5 class="mt-2 mb-0 fw-bold">{{number_format($data["totalYear"], 0, '.', '.');}} VNĐ</h5>
                           </div>
-                          <!--end col-->
                           <div class="col-3 align-self-center">
                             <div class="d-flex justify-content-center align-items-center thumb-xl bg-light rounded-circle mx-auto">
                               <i class="fa-regular fa-money-bill-1" style="color: green"></i>
                             </div>
                         </div>
-                          <!--end col-->
                       </div>
-                      <!--end row-->
-                  
                   </div>
-                  <!--end card-body-->
               </div>
-              <!--end card-->
             </div>
-            <!--end col-->
         </div>
         <!--end row-->
+        <?php
+          $months = [
+              "1"=> "Tháng 1" , 
+              "2"=> "Tháng 2" , 
+              "3"=> "Tháng 3" , 
+              "4"=> "Tháng 4" , 
+              "5"=> "Tháng 5" , 
+              "6"=> "Tháng 6" , 
+              "7"=> "Tháng 7" , 
+              "8"=> "Tháng 8" , 
+              "9"=> "Tháng 9" , 
+              "10"=> "Tháng 10" , 
+              "11"=> "Tháng 11" , 
+              "12"=> "Tháng 12" , 
+          ];
+          $years = [2020 , 2021 , 2022 , 2023 , 2024];
+        ?>
         <div class="row justify-content-center">
             <div class="col-md-6 col-lg-8">
                 <div class="card">
                     <div class="card-header">
-                        <div class="row align-items-center">
+                        <form method="GET" class="row align-items-center">
                           <div class="col-5">
-                            <select name="" id="" class="form-select">
-                              <option value="">Tháng 1</option>
-                              <option value="">Tháng 2</option>
-                              <option value="">Tháng 3</option>
-                              <option value="">Tháng 4</option>
-                              <option value="">Tháng 5</option>
+                            <select name="month" id="monthSelect" class="form-select">
+                              <option value="">---chọn tháng---</option>
+                              @foreach ($months as $key => $value)
+                              <option value="{{$key}}"  @selected($key == request('month'))>{{$value}}</option>
+                              @endforeach
                             </select>
                           </div>
                           <div class="col-5">
-                            <select name="" id="" class="form-select">
-                              <option value="">2021</option>
-                              <option value="">2022</option>
-                              <option value="">2023</option>
-                              <option value="">2024</option>
-                              <option value="">2025</option>
-                            </select>
+                            <select name="year" id="yearSelect" class="form-select">
+                              <option value="">--chọn năm ---</option>
+                              @foreach ($years as $year)
+                              <option value="{{$year}}"  @selected($year == request('year'))>{{$year}}</option>
+                              @endforeach
+                          </select>
                           </div>
                           <button class="col-2 btn btn-primary">Tìm kiếm</button>
-                        </div>
-                        <!--end row-->
+                        </form>
                     </div>
-                    <!--end card-header-->
-                    <div class="card-body pt-0">
-                        <div id="audience_overview" class="apex-charts"></div>
-                    </div>
-                    <!--end card-body-->
+                    
                 </div>
-                <!--end card-->
             </div>
-            <!--end col-->
             <div class="col-md-6 col-lg-4">
                 <div class="card">
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col" >
                                 <p class="text-dark mb-0 fw-semibold fs-14">Doanh thu hôm nay</p>
-                                <h4 class="mt-2 mb-0 fw-bold" style="color: green">1.000.000 VNĐ</h4>
+                                <h4 class="mt-2 mb-0 fw-bold" style="color: green">{{number_format($data["totalToday"], 0, '.', '.');}} VNĐ</h4>
                             </div>
-                            <!--end col-->
                             <div class="col-3 align-self-center">
                               <div class="d-flex justify-content-center align-items-center thumb-xl bg-light rounded-circle mx-auto">
                                 <i class="fa-regular fa-money-bill-1" style="color: green"></i>
                               </div>
                           </div>
-                            <!--end col-->
                         </div>
-                        <!--end row-->
-                    
                     </div>
-                    <!--end card-body-->
                 </div>
-                <!--end card-->
             </div>
-            <!--end col-->
         </div>
         <!--end row-->
         <div class="row">
@@ -189,33 +164,19 @@
             </div>
           </div>
         </div>
-        
-
-    </div><!-- container -->
-
-    <!--Start Rightbar-->
-    <!--Start Rightbar/offcanvas-->
-
+    </div>
     <script src="{{ asset('backend/assets/libs/chart.js/chart.min.js') }}"></script>
     <script>
+        const dataChart =  @json($data['dataChart']);
         var lineChart = document.getElementById("lineChart").getContext("2d"),
             barChart = document.getElementById("barChart").getContext("2d")
         var myLineChart = new Chart(lineChart, {
             type: "line",
             data: {
                 labels: [
-                    "tháng 1",
-                    "tháng 2",
-                    "tháng 3",
-                    "tháng 4",
-                    "tháng 5",
-                    "tháng 6",
-                    "tháng 7",
-                    "tháng 8",
-                    "tháng 9",
-                    "tháng 10",
-                    "tháng 11",
-                    "tháng 12",
+                  ...dataChart.map(item => {
+                        return  item.date
+                    })
                 ],
                 datasets: [{
                     label: "Đơn hàng",
@@ -230,7 +191,9 @@
                     fill: true,
                     borderWidth: 2,
                     data: [
-                        542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 900,
+                      ...dataChart.map(item => {
+                        return  item.order
+                    })
                     ],
                 }, ],
             },
@@ -267,24 +230,17 @@
             type: "bar",
             data: {
                 labels: [
-                    "tháng 1",
-                    "tháng 2",
-                    "tháng 3",
-                    "tháng 4",
-                    "tháng 5",
-                    "tháng 6",
-                    "tháng 7",
-                    "tháng 8",
-                    "tháng 9",
-                    "tháng 10",
-                    "tháng 11",
-                    "tháng 12",
+                  ...dataChart.map(item => {
+                        return  item.date
+                    })
                 ],
                 datasets: [{
                     label: "Doanh thu",
                     backgroundColor: "rgb(23, 125, 255)",
                     borderColor: "rgb(23, 125, 255)",
-                    data: [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4],
+                    data: [...dataChart.map(item => {
+                        return  item.total
+                    })],
                 }, ],
             },
             options: {
