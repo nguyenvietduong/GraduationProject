3b80b6ff4d5f28ea60ae145dacf637bfd6cfdaa5
CuongNguyen1308 <146540710+CuongNguyen1308@users.noreply.github.com>
2024-11-26 15:02:53 +0700

    Update promotion - use voucher

--- app/Http/Controllers/Backend/Promotion/AjaxPromotion.php
+++ app/Http/Controllers/Backend/Promotion/AjaxPromotion.php
@@ -25,17 +25,59 @@ class AjaxPromotion extends Controller
     }
     public function getDetailVoucher(Request $request)
     {
-        $count = 0;
-        $promotion = Promotion::where("code", $request->get("code"))->first();
-        if (isset($promotion)) {
-            $count = PromotionUser::where('promotion_id', $promotion->id)->count();
+        // Lấy thông tin mã giảm giá
+        $promotion = Promotion::where("id", $request->get("id"))
+            ->where("start_date", "<=", Carbon::now())
+            ->where("end_date", ">", Carbon::now())
+            ->where("is_active", 1)
+            ->first();
+
+        if (!$promotion) {
+            // Mã giảm giá không tồn tại hoặc không hợp lệ
+            return response()->json([
+                'message' => 'Mã giảm giá không tồn tại hoặc không hợp lệ.',
+                'data' => null
+            ]);
         }
-        return  Promotion::where("code", $request->get("code"))
+
+        // Đếm số lượng người dùng đã sử dụng mã
+        $count = PromotionUser::where("promotion_id", $promotion->id)->count();
+
+        // Kiểm tra các điều kiện bổ sung
+        if (
+            $promotion->total > $count && // Tổng số mã chưa bị vượt quá
+            $promotion->min_order_value <= $request->query("totalAmount") // Giá trị đơn hàng tối thiểu
+        ) {
+            // Mã hợp lệ
+            return response()->json([
+                'message' => 'Mã giảm giá hợp lệ.',
+                'data' => $promotion
+            ]);
+        }
+
+        // Mã giảm giá không thỏa mãn điều kiện
+        return response()->json([
+            'message' => 'Mã giảm giá không thỏa mãn điều kiện sử dụng.',
+            'data' => null
+        ]);
+    }
+    public function searchVoucher(Request $request)
+    {
+        return  Promotion::where(function ($query) use ($request) {
+            $query->where('code', $request->get("code"))
+                ->orWhere('title', 'LIKE', '%' . $request->get("code") . '%');
+        })
             ->where("start_date", '<=', Carbon::now())
             ->where("end_date", '>', Carbon::now())
             ->where("is_active", '=', 1)
-            ->where("total", '>', $count)
-            ->where("min_order_value", "<=",  $request->query("totalAmount"))
+            ->get();
+    }
+    public function getAllVoucher(Request $request)
+    {
+        return  Promotion::where("start_date", '<=', Carbon::now())
+            ->where("end_date", '>', Carbon::now())
+            ->where("is_active", '=', 1)
+            ->take(5)
             ->get();
     }
 }
