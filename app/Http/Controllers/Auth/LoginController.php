91c1307fb2bfd8a78330de20daaf6bc351baff5c
nguyenvietduong <duongnv10504@gmail.com>
2025-01-14 22:44:35 +0700

    Update login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
@@ -20,6 +21,41 @@ class LoginController extends Controller
     */
 
     use AuthenticatesUsers;
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $user = User::where($this->username(), $request->{$this->username()})->first();
+
+        if ($user && $user->status == 'locked') {
+            return back()->with('error', 'Tài khoản của bạn đã bị khóa.');
+        }
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            if ($request->hasSession()) {
+                $request->session()->put('auth.password_confirmed_at', time());
+            }
+
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
 
     protected function sendFailedLoginResponse(Request $request)
     {
